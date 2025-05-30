a2f14efe902837568c5aa5836271fe1b91e92f74
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-30 16:55:32 +0300

    refactor: useEffect intro commented out

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import Todo from "./components/Todo.tsx";
+// import {useEffect, useState} from "react";
 
 function App() {
+  // const [state, setState] = useState("")
+  // // Πυροδοτούμε το setup όταν ικανοποιηθεί το dependancy(optional / μπορεί να είναι και array)
+  // useEffect(setup, [state])
 
   return (
     <>
