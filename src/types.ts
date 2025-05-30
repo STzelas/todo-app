480330aca52e135bc9c6635452aa64a4bbebc644
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-30 16:54:28 +0300

    feat: complete type added

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,14 @@
 type TodoProps = {
   id: number;
   text: string;
+  completed: boolean;
 }
 
 type Action =
   |{type: "ADD"; payload: string}
   |{type: "DELETE"; payload: number}
-  |{type: "EDIT"; payload: {id: number, newText: string}};
+  |{type: "EDIT"; payload: {id: number, newText: string}}
+  |{type: "COMPLETE"; payload: number}
 
 type TodoFormProps = {
   dispatch: React.Dispatch<Action>
