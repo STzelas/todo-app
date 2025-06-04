01db30af64cc5506c12699265a44ca638ed99faa
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-04 20:30:51 +0300

    feat: new action added

--- src/types.ts
+++ src/types.ts
@@ -9,6 +9,7 @@ type Action =
   |{type: "DELETE"; payload: number}
   |{type: "EDIT"; payload: {id: number, newText: string}}
   |{type: "COMPLETE"; payload: number}
+  |{type: "CLEAR_ALL"}
 
 type TodoFormProps = {
   dispatch: React.Dispatch<Action>
